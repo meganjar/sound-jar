c02fd1243651aad47a528532448a2f065ac67c58
meganjar <68120966+meganjar@users.noreply.github.com>
2025-01-16 16:40:53 -0700

    import authUrl and add oauth DOM js to index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// import * as spotifyApi from "./api/spotifyApi";
-// import {clientID, clientSecret} from "./utils/constants";
+
 import { authUrl } from './api/spotifyAuth.js';
 
+
+// oauth DOM
 const authLinkEl = document.getElementById('auth-url');
 authLinkEl.href = authUrl;
 
+// about me DOM
 
-// const customInstance = axios.create({
-//     baseURL: 'https://api.spotify.com/v1',
-//     headers: {
-//         'Authorization': 'Bearer ${clientSecret}',
-//     },
-// });
+
+// create playlist DOM
+
+
