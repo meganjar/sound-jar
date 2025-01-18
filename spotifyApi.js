f1e0a2be190de947650d32d2144becadb15eaf6f
meganjar <68120966+meganjar@users.noreply.github.com>
2025-01-17 19:32:02 -0700

    separate JS into 3 modules

--- spotifyApi.js
+++ spotifyApi.js
@@ -1,44 +1,11 @@
 
-import { init, axiosInstance } from "./spotifyAuth";
-console.log("hi")
+// On the roadmap is another function that will make a post req :
 
-async function getTopSongs() {
-    try {
-        axiosInstance.get('/me/top/tracks')
-        console.log(response);
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-init();
-getTopSongs();
-getAboutMe();
-
-async function getAboutMe() {
-    try {
-        const axiosInstance = getAxiosInstance();
-        const response = await axiosInstance.get('/me');
-        return response.data; 
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
-
-
-
-
-async function createPlaylist() {
-    try {
-        const playlist = await axiosInstance.post('/users/{user_id}/playlists', {
-            name: 'From the sound jar',
-            description: 'new tracks you didn’t ask for but will definitely want.',
-            public: true
-        });
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-
-}
+// async function createPlaylist() {
+//     try {
+//         const playlist = await axiosInstance.post('/users/{user_id}/playlists', {
+//             name: 'From the sound jar',
+//             description: 'new tracks you didn’t ask for but will definitely want.',
+//             public: true
+//         });
+//     } catch (error) {
